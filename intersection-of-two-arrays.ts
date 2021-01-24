eca92bc60049cbc0034d75ff1dc466d293acbb71
sajithahd <hdsajitha@gmail.com>
2021-01-24 22:13:39 +0530

    1 file modified

--- intersection-of-two-arrays.ts
+++ intersection-of-two-arrays.ts
@@ -7,8 +7,8 @@ intersection.innerHTML += ` <br/> Single number is: ${intersect(
 )} <br/>`;
 
 intersection.innerHTML += `Single number is: ${intersect(
-  [2],
-  [1, 2, 2, 1]
+  [1, 2, 2, 1],
+  [2]
 )} <br/>`;
 
 intersection.innerHTML += `Single number is: ${intersect(
@@ -18,17 +18,10 @@ intersection.innerHTML += `Single number is: ${intersect(
 
 function intersect(nums1: number[], nums2: number[]): number[] {
   let arr = new Array<number>();
-  if (nums1.length > nums2.length) {
-    for (let i = 0; i < nums2.length; i++) {
-      if (nums1.includes(nums2[i])) {
-        arr.push(nums2[i]);
-      }
-    }
-  } else {
-    for (let i = 0; i < nums1.length; i++) {
-      if (nums2.includes(nums1[i])) {
-        arr.push(nums1[i]);
-      }
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums2.includes(nums1[i])) {
+      arr.push(nums2.splice(nums2.indexOf(nums1[i]),1)[0]);
+      ;
     }
   }
 
