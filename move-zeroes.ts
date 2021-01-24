06e2818fdf35d86e6efd0cc05df934b83a2c09e7
sajithahd <hdsajitha@gmail.com>
2021-01-24 23:57:11 +0530

    1 file modified

--- move-zeroes.ts
+++ move-zeroes.ts
@@ -18,15 +18,11 @@ moveZerosDiv.innerHTML += `<br/> Single number is: ${moveZeroes([
 
 function moveZeroes(nums: number[]): number[] {
   //[0,0,1] -> [1,0,0]
-  let i = 0;
   for(let i=0 ;i<nums.length; i++){
     if (nums[i] == 0) {
       console.log(nums[i]);
       nums.splice(i, 1);
-      nums.push(0);
-       i++;
-    } else {
-      i++;
+      nums.push(0);       
     }
   }
 
