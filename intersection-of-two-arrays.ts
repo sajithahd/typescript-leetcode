b832d8451b0c1ca1318b91f6fb745d92aeff4907
sajithahd <hdsajitha@gmail.com>
2021-01-24 21:00:41 +0530

    1 file modified

--- intersection-of-two-arrays.ts
+++ intersection-of-two-arrays.ts
@@ -1,21 +1,36 @@
 // Write TypeScript code!
 const intersection: HTMLElement = document.getElementById("intersection");
 
-intersection.innerHTML += `Single number is: ${intersect([2, 2, 1])} <br/>`;
-intersection.innerHTML += `Single number is: ${intersect([4, 1, 2, 1, 2])} <br/>`;
-intersection.innerHTML += `Single number is: ${intersect([1, 3, 1])} <br/>`;
+intersection.innerHTML += `Single number is: ${intersect(
+  [1, 2, 2, 1],
+  [2, 2]
+)} <br/>`;
 
-function intersect(nums: number[]): number {
-  let i = 0;
-  nums.sort();
-  while (i < nums.length) {
-    let n = nums.splice(i, 1)[0];
-    console.log(n);
-    console.log(nums);
+intersection.innerHTML += `Single number is: ${intersect(
+  [2],
+  [1, 2, 2, 1]
+)} <br/>`;
 
-    if (nums.indexOf(n) < 0) {
-      return n;
-    } 
-    i++;
+intersection.innerHTML += `Single number is: ${intersect(
+  [3, 1, 2],
+  [1, 1]
+)} <br/>`;
+
+function intersect(nums1: number[], nums2: number[]): number[] {
+  let arr = new Array<number>();
+  if (nums1.length > nums2.length) {
+    for (let i = 0; i < nums2.length; i++) {
+      if (nums1.includes(nums2[i])) {
+        arr.push(nums2[i]);
+      }
+    }
+  } else {
+    for (let i = 0; i < nums1.length; i++) {
+      if (nums2.includes(nums1[i])) {
+        arr.push(nums1[i]);
+      }
+    }
   }
+
+  return arr;
 }
