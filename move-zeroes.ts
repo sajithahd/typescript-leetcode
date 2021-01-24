1d7c63d0c1eb3a23cba94294a908589ed1cd6908
sajithahd <hdsajitha@gmail.com>
2021-01-24 23:39:48 +0530

    1 file modified

--- move-zeroes.ts
+++ move-zeroes.ts
@@ -1,25 +1,33 @@
 // Write TypeScript code!
 const moveZerosDiv: HTMLElement = document.getElementById("move-zeros");
 
-moveZerosDiv.innerHTML += ` <br/> Single number is: ${moveZeroes(
-  [0,1,0,3,12]
-)} <br/>`;
+moveZerosDiv.innerHTML += ` <br/> Single number is: ${moveZeroes([
+  0,
+  1,
+  0,
+  3,
+  12
+])} <br/>`;
 
-moveZerosDiv.innerHTML += `<br/> Single number is: ${moveZeroes(
-  [0,0,1,2]
-)} <br/>`;
+moveZerosDiv.innerHTML += `<br/> Single number is: ${moveZeroes([
+  0,
+  0,
+  1,
+  2
+])} <br/>`;
 
 function moveZeroes(nums: number[]): number[] {
-   //[0,0,1] -> [1,0,0]
-   
-    for(let i=0 ;i<nums.length; i++){
-        if(nums[i]==0){
-            console.log(nums[i]);
-            nums.splice(i,1);
-            nums.push(0);
-            i=0;
-        }
+  //[0,0,1] -> [1,0,0]
+  let i = 0;
+  while (i < nums.length) {
+    if (nums[i] == 0) {
+      console.log(nums[i]);
+      nums.splice(i, 1);
+      nums.push(0);
+    } else{
+      i++;
     }
+  }
 
-    return nums
-};
+  return nums;
+}
